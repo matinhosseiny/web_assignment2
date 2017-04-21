8dbfe7a08c6b409779981098647411773fb9d3ad
matin <matinhosseiny@gmail.com>
2017-04-21 21:02:19 +0000

    finish sign_up and ssl

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 root 'static_pages#home'
 get  '/signup',  to: 'users#new'
 resources :users
+post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
