b7125519a7a449d757d2bdd7268746e77a24df51
matin <matinhosseiny@gmail.com>
2017-04-21 22:18:37 +0000

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   resources :users
 root 'static_pages#home'
 get  '/signup',  to: 'users#new'
 resources :users
 post '/signup',  to: 'users#create'
+get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
